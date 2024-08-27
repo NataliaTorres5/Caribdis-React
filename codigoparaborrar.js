82fe849854cce2d0bffb5f84e9e7ba2770f03c21
NataliaTorres5 <nataliatorres88@gmail.com>
2024-08-26 23:09:29 -0500

    update in footer

--- codigoparaborrar.js
+++ codigoparaborrar.js
@@ -1,5 +1,5 @@
 
-
+<>
 <footer class="footer">
 
 <div class="footer-top section">
@@ -194,3 +194,68 @@
 </div>
 
 </footer>
+
+
+
+<div>
+<a href=".com" className="logo">
+  <img src="" alt="" />
+</a>
+</div>
+<div className="social-list">
+<ul className="social-list">
+  <li className="social-link">Facebook</li>
+  <li className="social-link">X</li>
+  <li className="social-link"> Instagram</li>
+</ul>
+</div>
+
+<div className="footer-link-box">
+<ul className="footer-list">
+  <li className="footer-list-title">
+    <img src="" alt="" />
+    <span className="footer-link-text">
+      275 Carobdis 134, Colombia
+    </span>
+  </li>
+  <li className="footer-list-title">
+    <img src="" alt="" />
+    <span className="footer-link-text">
+      275 Carobdis 134, Colombia
+    </span>
+  </li>
+  <li className="footer-list-title">
+    <img src="" alt="" />
+    <span className="footer-link-text">
+      275 Carobdis 134, Colombia
+    </span>
+  </li>
+
+</ul>
+
+<ul className="footer-list">
+  <li className="footer-list-title">
+    <img src="" alt="" />
+    <span className="footer-link-text">
+      275 Carobdis 134, Colombia
+    </span>
+  </li>
+  <li className="footer-list-title">
+    <img src="" alt="" />
+    <span className="footer-link-text">
+      275 Carobdis 134, Colombia
+    </span>
+  </li>
+  <li className="footer-list-title">
+    <img src="" alt="" />
+    <span className="footer-link-text">
+      275 Carobdis 134, Colombia
+    </span>
+  </li>
+
+</ul>
+
+
+</div>
+
+</>
