3408011e7364f3812010712c56a2d29af051c14c
NataliaTorres5 <nataliatorres88@gmail.com>
2024-09-01 23:50:35 -0500

    Created User page

--- src/pages/SignInSignUpPage.jsx
+++ src/pages/SignInSignUpPage.jsx
@@ -1,7 +1,62 @@
-import React from 'react'
+import React, { useState } from "react";
+import SignIng from '../components/SignIng'
+import LogIn from '../components/LogIn'
+import '../styles/SignInSignUp.css'
 
 export default function SignInSignUpPage() {
-  return (
-    <div>SignInSignUpPage</div>
-  )
-}
+    const [isActive, setIsActive] = useState(false);
+    const [isForm, setIsForm] =useState(false);
+
+
+    const toggleOverlay = () => {
+        setIsActive(!isActive);
+        setIsForm(!isForm);
+
+        
+
+
+
+
+};
+    return (
+        <div>
+
+            <div className="cointainer-signin">
+
+
+                <div className="overlay box">
+
+                    <button onClick={toggleOverlay}>
+                       {isForm ? 'Registro' : 'Ingresa'}
+                        
+                          </button>
+
+
+{isActive ? ( 
+
+<SignIng/>
+
+):(
+
+<LogIn/>
+) }
+
+
+                </div>
+
+
+
+
+            </div>
+    
+    
+    
+    
+    
+        </div>
+      )
+
+    
+
+  }
+
